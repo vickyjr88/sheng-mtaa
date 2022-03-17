67764e343ec961b14da122f3d3d15d95c77bcdd8
Elinah Wanjiru <elinahwanjiru20@gmail.com>
2022-03-17 11:56:08 +0300

    m

--- src/components/Like.js
+++ src/components/Like.js
@@ -2,8 +2,11 @@ import React, { useState } from "react"
 
 var Like = () => { 
 const [count, setCount] = useState(0)
-    return (
-        <button onClick={() => setCount(count + 1)}>Likes: {count}</button>
+    return ( <>
+        <button onClick={() => setCount(count +1)}>Like</button>
+        <button onclick>{count} </button> 
+    
+        </>
   )
 }
 export default Like
