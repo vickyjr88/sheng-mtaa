8dbd5ad258d4ab44cbfd93fe46addc0be46d832c
Ian Irungu <gatumaian@gmail.com>
2022-03-17 12:43:44 +0300

    Added post and get methods waiting foe Api file.

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,12 +1,42 @@
-import React, { useState } from "react"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
 
 var Like = () => { 
-const [count, setCount] = useState(0)
-    return ( <>
+    const [count, setCount] = useState(0)
+
+    useEffect(() =>
+      axios.post('/api/private/likes')
+          .then(res => {
+              console.log(res)
+              
+              })
+          .catch(err => {
+              console.log(err)
+             })
+          )
+  
+    useEffect(() =>
+      axios.get('/api/private/likes')
+          .then(res => {
+              console.log(res)
+              setCount(res.data)
+              })
+          .catch(err => {
+              console.log(err)
+             })
+          )
+
+
+  return ( 
+        <>
+
         <button onClick={() => setCount(count +1)}>Like</button>
-        <button onclick>{count} </button> 
-    
+        
+        <button> { count } </button>
+
         </>
+
+    
   )
 }
 export default Like
