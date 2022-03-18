6088f4194d0cfc7063258f07b6da93f43a1dcf20
Ian Irungu <gatumaian@gmail.com>
2022-03-18 10:43:14 +0300

    Enhanced login

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ function Login () {
 
     return(<>
         <div className="col-sm-6 offset-sm-3">
-            <h4>Register new Account</h4>
+            <h4>Login to your account</h4>
             <label> Username/ Nickname </label><br/>
             <input type="text" id="username" onChange={(e) =>setUser(e.target.value)} placeholder="username"
                value={user} required/><br/>
@@ -19,7 +19,7 @@ function Login () {
             <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}
                value={password} required/><br/> 
 
-            <button className="col-sm-3 offset-sm-1 btn-primary" onClick={login}>Sign Up</button>
+            <button className="col-sm-3 offset-sm-1 btn-primary" onClick={login}>login</button>
               <br/><br/>           
         </div>
         <div className="col-sm-6 offset-sm-3">
