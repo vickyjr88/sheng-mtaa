44a4d256bc2f07950c1d931090e3e88f6bebbe7b
Victor Karanja <vickyjr888@gmail.com>
2022-04-30 22:34:09 +0300

    Remove login link.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,7 +11,6 @@ const Navigation = () => {
                         <Nav.Link href='/'>Sheng</Nav.Link>
                         <Nav.Link href='/about'>About</Nav.Link>
                         <Nav.Link href='/mchongoanos'>Mchongoano</Nav.Link>
-                        <Nav.Link href='/login'>Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
